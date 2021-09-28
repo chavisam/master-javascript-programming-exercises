0d1e5e1c4d1e5aa041e08458a035c1bde99f950a
Javier <jmanteca@jmanvel.com>
2021-09-28 21:16:57 +0000

    157 hechos

--- exercises/156-findPairForSum/app.js
+++ exercises/156-findPairForSum/app.js
@@ -1,5 +1,23 @@
 function findPairForSum(array, number){
-  
+    let arr = []
+ for (let i = 0; i < array.length; i++) 
+ {
+     
+     const num1 = array[i];
+     
+     for (let y = 1; y < array.length-1; y++) {
+         const num2 = array[y];
+
+         if(num1+num2==number)
+         {
+             arr.push(num1);
+             arr.push(num2);
+             return arr
+         }else{}
+         
+     }
+ }
+ 
 }
 
 var pair = findPairForSum([3, 34, 4, 12, 5, 2], 9);
--- exercises/157-isRotated/app.js
+++ exercises/157-isRotated/app.js
@@ -1,6 +1,20 @@
 function isRotated(str1, str2) {
-    // your code here
+   
+
+   for (let i = 0; i < str1.length; i++) {
+       
+       let end = str1.slice(0,i)
+       let  word = str1.slice(i,9999)
+       
+       let output = word + end
+
+   //comparamos si es igual a str2
+     if(output == str2)
+     {return true}
+     
+   }  
+   return false
 }
 
 console.log(isRotated('hello world', 'orldhello w')) // => true
-console.log(isRotated('hello world', 'omrel wp')) // => false
+//console.log(isRotated('hello world', 'omrel wp')) // => false
